a4c655403f5d777fe1deb89404ba4a75ef73eb67
Joan Andrés <andres6936@live.com>
2020-12-29 13:51:48 -0500

    Deleted unused include

--- Source/Geometry/Coordinate.cpp
+++ Source/Geometry/Coordinate.cpp
@@ -13,10 +13,8 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License 
 along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
-#include "stdafx.hpp"
 
 #include <boost/functional/hash.hpp>
-
 #include "Goblin/Geometry/Coordinate.hpp"
 
 std::size_t hash_value(const Coordinate& coord) {
