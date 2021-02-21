cc0cf090916ffd697090a4c968a48bca4d7e199a
Joan Andrés <andres6936@live.com>
2021-02-21 07:10:21 -0500

    Removed unused include

--- Source/Announce.cpp
+++ Source/Announce.cpp
@@ -13,7 +13,6 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License 
 along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
-#include "stdafx.hpp"
 
 #include "Goblin/User/Interface/Announce.hpp"
 #include "GCamp.hpp"
