6a70431f209304474ccb9efcbbae6c1f877f7819
Joan Andrés <andres6936@live.com>
2021-02-21 07:11:49 -0500

    Reformat code

--- Source/Announce.cpp
+++ Source/Announce.cpp
@@ -20,86 +20,115 @@ along with Goblin Camp. If not, see <http://www.gnu.org/licenses/>.*/
 #include "Game.hpp"
 
 AnnounceMessage::AnnounceMessage(std::string nmsg, TCODColor col, Coordinate pos) :
-msg(nmsg),
-	counter(1),
-	color(col),
-	target(pos) {}
+		msg(nmsg),
+		counter(1),
+		color(col),
+		target(pos)
+{
+}
 
-std::string AnnounceMessage::ToString() {
+std::string AnnounceMessage::ToString()
+{
 	result.str("");
-	result<<msg;
-	if (counter > 1) {
-		result<<" x"<<counter;
+	result << msg;
+	if (counter > 1)
+	{
+		result << " x" << counter;
 	}
-	if (target.X() > -1 && target.Y() > -1) {
-		result<<" "<<(char)TCOD_CHAR_ARROW_E;
+	if (target.X() > -1 && target.Y() > -1)
+	{
+		result << " " << (char)TCOD_CHAR_ARROW_E;
 	}
 	return result.str();
 }
 
 Announce* Announce::instance = 0;
 
-Announce* Announce::Inst() {
+Announce* Announce::Inst()
+{
 	if (!instance) instance = new Announce();
 	return instance;
 }
 
 Announce::Announce() :
-timer(0),
-	length(0),
-	height(0)
-{}
+		timer(0),
+		length(0),
+		height(0)
+{
+}
 
-void Announce::AddMsg(std::string msg, TCODColor color, const Coordinate& coordinate) {
-	msg = msg.substr(0,ANNOUNCE_MAX_LENGTH);
-	if (!messageQueue.empty() && messageQueue.back()->msg == msg && messageQueue.back()->target == coordinate) {
+void Announce::AddMsg(std::string msg, TCODColor color, const Coordinate& coordinate)
+{
+	msg = msg.substr(0, ANNOUNCE_MAX_LENGTH);
+	if (!messageQueue.empty() && messageQueue.back()->msg == msg &&
+		messageQueue.back()->target == coordinate)
+	{
 		messageQueue.back()->counter++;
 		if (messageQueue.size() == 1) timer = 0;
-	} else {
+	}
+	else
+	{
 		messageQueue.push_back(new AnnounceMessage(msg, color, coordinate));
-		if (messageQueue.size() <= ANNOUNCE_HEIGHT) {
+		if (messageQueue.size() <= ANNOUNCE_HEIGHT)
+		{
 			if (msg.length() > length) length = msg.length();
 			height = messageQueue.size();
 		}
 	}
 }
 
-void Announce::Update() {
-	if (!messageQueue.empty()) {
+void Announce::Update()
+{
+	if (!messageQueue.empty())
+	{
 		++timer;
-		if (timer > 0.5*UPDATES_PER_SECOND) {
-			if (messageQueue.size() > (unsigned int)ANNOUNCE_HEIGHT) {
+		if (timer > 0.5 * UPDATES_PER_SECOND)
+		{
+			if (messageQueue.size() > (unsigned int)ANNOUNCE_HEIGHT)
+			{
 				history.push_back(messageQueue.front());
-				if (history.size() > 1000) {
-					delete(history.front());
+				if (history.size() > 1000)
+				{
+					delete (history.front());
 					history.pop_front();
 				}
 				messageQueue.pop_front();
 				length = 0;
-				for (std::deque<AnnounceMessage*>::iterator msgi = messageQueue.begin(); msgi != messageQueue.end(); ++msgi) {
+				for (std::deque<AnnounceMessage*>::iterator msgi = messageQueue.begin();
+					 msgi != messageQueue.end(); ++msgi)
+				{
 					if ((*msgi)->msg.length() > length) length = (*msgi)->msg.length();
 				}
 			}
 			timer = 0;
 		}
-	} else timer = 0;
+	}
+	else timer = 0;
 }
 
-void Announce::AnnouncementClicked(AnnounceMessage *msg) {
-	if(msg->target.X() > -1 && msg->target.Y() > -1) {
+void Announce::AnnouncementClicked(AnnounceMessage* msg)
+{
+	if (msg->target.X() > -1 && msg->target.Y() > -1)
+	{
 		Game::Inst()->CenterOn(msg->target);
 	}
 }
 
-void Announce::AnnouncementClicked(int i) {
-	if(i >= 0 && i < (signed int)history.size()) {
+void Announce::AnnouncementClicked(int i)
+{
+	if (i >= 0 && i < (signed int)history.size())
+	{
 		AnnouncementClicked(history[i]);
 	}
 }
 
-MenuResult Announce::Update(int x, int y, bool clicked) {
-	if(x < (signed int)length + 6 && y >= (signed int)top) {
-		if(clicked && y > (signed int)top && (y - top - 1) >= 0 && (y - top - 1) < messageQueue.size()) {
+MenuResult Announce::Update(int x, int y, bool clicked)
+{
+	if (x < (signed int)length + 6 && y >= (signed int)top)
+	{
+		if (clicked && y > (signed int)top && (y - top - 1) >= 0 &&
+			(y - top - 1) < messageQueue.size())
+		{
 			AnnouncementClicked(messageQueue[y - top - 1]);
 		}
 		return MENUHIT;
@@ -107,45 +136,58 @@ MenuResult Announce::Update(int x, int y, bool clicked) {
 	return NOMENUHIT;
 }
 
-void Announce::Draw(TCODConsole* console) {
+void Announce::Draw(TCODConsole* console)
+{
 	console->setAlignment(TCOD_LEFT);
 
 	top = console->getHeight() - 1 - height;
-	if (height > 0 && (signed int)height < console->getHeight() - 1) {
-		console->hline(0, top, length+6);
-		console->putChar(length+5, top, TCOD_CHAR_NE, TCOD_BKGND_SET);
-		console->vline(length+5, top + 1, height);
-		console->rect(0, top + 1, length+5, height, true);
+	if (height > 0 && (signed int)height < console->getHeight() - 1)
+	{
+		console->hline(0, top, length + 6);
+		console->putChar(length + 5, top, TCOD_CHAR_NE, TCOD_BKGND_SET);
+		console->vline(length + 5, top + 1, height);
+		console->rect(0, top + 1, length + 5, height, true);
 
-		for (int i = std::min((int)messageQueue.size() - 1, (int)height-1); i >= 0; --i) {
+		for (int i = std::min((int)messageQueue.size() - 1, (int)height - 1); i >= 0; --i)
+		{
 			AnnounceMessage* msg = messageQueue[i];
 			console->setDefaultForeground(msg->color);
-			console->print(0, console->getHeight()-(height-i), msg->ToString().c_str());
+			console->print(0, console->getHeight() - (height - i), msg->ToString().c_str());
 		}
 	}
 }
 
-void Announce::Draw(Coordinate pos, int from, int amount, TCODConsole* console) {
+void Announce::Draw(Coordinate pos, int from, int amount, TCODConsole* console)
+{
 	int count = 0;
-	for (std::deque<AnnounceMessage*>::reverse_iterator ani = messageQueue.rbegin(); ani != messageQueue.rend(); ++ani) {
-		if (count++ >= from) {
-			console->print(pos.X(), pos.Y()+count-from, (*ani)->ToString().c_str());
-			if (count-from+1 == amount) return;
+	for (std::deque<AnnounceMessage*>::reverse_iterator ani = messageQueue.rbegin();
+		 ani != messageQueue.rend(); ++ani)
+	{
+		if (count++ >= from)
+		{
+			console->print(pos.X(), pos.Y() + count - from, (*ani)->ToString().c_str());
+			if (count - from + 1 == amount) return;
 		}
 	}
-	for (std::deque<AnnounceMessage*>::reverse_iterator ani = history.rbegin(); ani != history.rend(); ++ani) {
-		if (count++ >= from) {
-			console->print(pos.X(), pos.Y()+count-from, (*ani)->ToString().c_str());
-			if (count-from+1 == amount) return;
+	for (std::deque<AnnounceMessage*>::reverse_iterator ani = history.rbegin();
+		 ani != history.rend(); ++ani)
+	{
+		if (count++ >= from)
+		{
+			console->print(pos.X(), pos.Y() + count - from, (*ani)->ToString().c_str());
+			if (count - from + 1 == amount) return;
 		}
 	}
 }
 
-void Announce::EmptyMessageQueue() {
-	while (!messageQueue.empty()) {
+void Announce::EmptyMessageQueue()
+{
+	while (!messageQueue.empty())
+	{
 		history.push_front(messageQueue.front());
-		if (history.size() > 1000) {
-			delete(history.back());
+		if (history.size() > 1000)
+		{
+			delete (history.back());
 			history.pop_back();
 		}
 		messageQueue.pop_front();
@@ -153,15 +195,18 @@ void Announce::EmptyMessageQueue() {
 	length = 0;
 }
 
-int Announce::AnnounceAmount() {
-	return history.size()+messageQueue.size();
+int Announce::AnnounceAmount()
+{
+	return history.size() + messageQueue.size();
 }
 
-Coordinate Announce::CurrentCoordinate() {
+Coordinate Announce::CurrentCoordinate()
+{
 	return messageQueue.front()->target;
 }
 
-void Announce::Reset() {
+void Announce::Reset()
+{
 	delete instance;
 	instance = 0;
 }
