149dbc9a84f0792b8be12bade1cb4567e9b79922
Joan Andrés <andres6936@live.com>
2021-02-21 21:43:31 -0500

    Separated the mix source code

---
+++ Include/Goblin/User/Interface/Scrollable.hpp
@@ -0,0 +1,34 @@
+// Joan Andrés (@Andres6936) Github.
+
+#ifndef GOBLINCAMP_SCROLLABLE_HPP
+#define GOBLINCAMP_SCROLLABLE_HPP
+
+/**
+ * Abstract class
+ */
+class Scrollable
+{
+
+public:
+
+	// Destructor
+
+	virtual ~Scrollable()
+	{
+	}
+
+	virtual void Draw(int x, int y, int scroll, int width, int height, TCODConsole*) = 0;
+
+	virtual int TotalHeight() = 0;
+
+	virtual MenuResult Update(int x, int y, bool clicked, TCOD_key_t key)
+	{
+		return NOMENUHIT;
+	}
+
+	virtual void GetTooltip(int x, int y, Tooltip* tooltip)
+	{
+	};
+};
+
+#endif //GOBLINCAMP_SCROLLABLE_HPP
---
+++ Include/Goblin/User/Interface/UIContainer.hpp
@@ -0,0 +1,31 @@
+// Joan Andrés (@Andres6936) Github.
+
+#ifndef GOBLINCAMP_UICONTAINER_HPP
+#define GOBLINCAMP_UICONTAINER_HPP
+
+class UIContainer : public Drawable
+{
+
+protected:
+
+	std::vector<Drawable*> components;
+
+public:
+
+	UIContainer(std::vector<Drawable*> ncomponents, int nx, int ny, int nwidth, int nheight) :
+			Drawable(nx, ny, nwidth, nheight), components(ncomponents)
+	{
+	}
+
+	~UIContainer();
+
+	void AddComponent(Drawable* component);
+
+	virtual void Draw(int, int, TCODConsole*);
+
+	virtual MenuResult Update(int, int, bool, TCOD_key_t);
+
+	virtual void GetTooltip(int, int, Tooltip*);
+};
+
+#endif //GOBLINCAMP_UICONTAINER_HPP
