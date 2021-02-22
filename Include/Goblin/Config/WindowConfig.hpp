7ff063db1fa253db3d3dc3bbfacb13e16cac802b
Joan Andrés <andres6936@live.com>
2021-02-21 19:42:42 -0500

    Added documentation

--- Include/Goblin/Config/WindowConfig.hpp
+++ Include/Goblin/Config/WindowConfig.hpp
@@ -8,27 +8,49 @@
 namespace Goblin
 {
 
+	/**
+	 * Store the information of main console, this information include the width
+	 * and height in cells of main console.
+	 */
 	class WindowConfig final
 	{
 
 	private:
 
+		/**
+		 * Width of main console in cells.
+		 */
 		inline static std::int32_t width{ 0 };
 
+		/**
+		 * Height of main console in cells.
+		 */
 		inline static std::int32_t height{ 0 };
 
 	public:
 
 		// Getters
 
+		/**
+		 * @return The width of main console in cells.
+		 */
 		static std::int32_t getWidth() noexcept;
 
+		/**
+		 * @return The height of main console in cells.
+		 */
 		static std::int32_t getHeight() noexcept;
 
 		// Setters
 
+		/**
+		 * @param width Set the width of main console in cells.
+		 */
 		static void setWidth(std::int32_t width) noexcept;
 
+		/**
+		 * @param height Set the height of main console in cells.
+		 */
 		static void setHeight(std::int32_t height) noexcept;
 
 	};
