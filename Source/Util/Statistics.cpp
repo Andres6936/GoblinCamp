4906ca94c9421fb894eee8d2dc8d063ef61ca2ff
Joan Andrés <andres6936@live.com>
2021-03-20 23:33:33 -0500

    Implemented methods

--- Source/Util/Statistics.cpp
+++ Source/Util/Statistics.cpp
@@ -4,3 +4,32 @@
 
 using namespace Goblin;
 
+void Statistics::RaiseOrcs(const std::uint32_t amount) noexcept
+{
+	orcs += amount;
+}
+
+void Statistics::RaiseGoblins(const std::uint32_t amount) noexcept
+{
+	goblins += amount;
+}
+
+std::uint32_t Statistics::GetOrcs() const noexcept
+{
+	return orcs;
+}
+
+std::uint32_t Statistics::GetGoblins() const noexcept
+{
+	return goblins;
+}
+
+std::uint32_t Statistics::GetPopulation() const noexcept
+{
+	return orcs + goblins;
+}
+
+std::uint32_t Statistics::GetProgressionLevel() const noexcept
+{
+	return 0;
+}
